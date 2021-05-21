cd1beadff9590d26a426c9779f94fe488a3d22b0
Bhama2413 <bhamapnair@gmail.com>
2021-05-20 21:09:47 -0400

    My second change

--- src/main/java/Practicemay18.java
+++ src/main/java/Practicemay18.java
@@ -1,7 +1,7 @@
 public class Practicemay18 {
     public static void main(String[] args) {
 
-        int[][] nums = {{1, 2}, {4, 9}, {9, 8}, {6, 7}};
+        int[][] nums = {{2, 2}, {4, 9}, {9, 8}, {6, 7}};
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < nums[i].length; j++) {
                 System.out.println(nums[i][j]);
