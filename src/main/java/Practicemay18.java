0f97c53a4d8a0a801e46b6356805381d69c51b9d
Bhama2413 <bhamapnair@gmail.com>
2021-05-20 20:21:59 -0400

    hello

---
+++ src/main/java/Practicemay18.java
@@ -0,0 +1,14 @@
+public class Practicemay18 {
+    public static void main(String[] args) {
+
+        int[][] nums = {{1, 2}, {4, 9}, {9, 8}, {6, 7}};
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i].length; j++) {
+                System.out.println(nums[i][j]);
+            }
+        }
+
+    }
+
+    }
+
